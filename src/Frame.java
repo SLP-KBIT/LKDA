2100ec15af16fe2722e9391afaaf72403aa8c5b2
sekishin <s14t242@stmail.eng.kagawa-u.ac.jp>
2015-09-25 15:13:10 +0900

    image output

--- src/Frame.java
+++ src/Frame.java
@@ -1,10 +1,12 @@
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
-public class Frame/* extends JFrame implements ActionListener*/ {
+public class Frame extends JFrame/*implements ActionListener*/ {
   JFrame content;
   JLabel tittle_logo;
   JButton finish;
@@ -14,14 +16,21 @@ public class Frame/* extends JFrame implements ActionListener*/ {
     content.setSize(1000,800);
     content.setVisible(true);
 
-    tittle_logo = new JLabel("knyn");
+    tittle_logo = new JLabel("knyn", SwingConstants.CENTER);
     tittle_logo.setFont(new Font("Century", Font.ITALIC, 300));
     tittle_logo.setBounds(700, 500, 200, 400);
-    content.add(tittle_logo);
+    //content.add(tittle_logo);
 
-    finish = new JButton(ene.name);
-    finish.setBounds(167, 220, 91, 21);
-    content.add(finish);
+    ImageIcon  img;
+    img = new ImageIcon(/*画像ファイル名*/);
+    JLabel label = new JLabel(img);
+    content.add(label);
+
+
+
+    //finish = new JButton(ene.name);
+    //finish.setBounds(167, 220, 91, 21);
+    //content.add(finish);
     //finish.addActionListener(this);
 
   }
