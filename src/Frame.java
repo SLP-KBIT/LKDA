b2802d16b2505f6e0c296ed93053c70e680ef07d
shivashin <s14t241@stmail.eng.kagawa-u.ac.jp>
2015-10-06 21:37:26 +0900

    確認用

--- src/Frame.java
+++ src/Frame.java
@@ -1,31 +1,37 @@
 import java.awt.Font;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 public class Frame extends JFrame/*implements ActionListener*/ {
   JFrame content;
   JLabel tittle_logo;
   JButton finish;
+  JPanel imgpanel;
+  GamePanel panel = new GamePanel();
 
   void createFrame (Enemy ene) {
     content = new JFrame("View JFrame");
-    content.setSize(1000,800);
+    content.setSize(800,800);
+    content.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     content.setVisible(true);
 
-    tittle_logo = new JLabel("knyn", SwingConstants.CENTER);
-    tittle_logo.setFont(new Font("Century", Font.ITALIC, 300));
-    tittle_logo.setBounds(700, 500, 200, 400);
-    //content.add(tittle_logo);
+    tittle_logo = new JLabel("lkda", SwingConstants.CENTER);
+    tittle_logo.setFont(new Font("Century", Font.ITALIC, 30));
+    tittle_logo.setBounds(70, 50, 20, 40);
+    content.add(tittle_logo);
 
+    imgpanel = panel.imgpanel();
+    content.add(imgpanel);
+/*
     ImageIcon  img;
-    img = new ImageIcon(/*画像ファイル名*/);
+    img = new ImageIcon("C:\\Eclipse\\LKDA\\bin\\lolicom.jpg");
     JLabel label = new JLabel(img);
     content.add(label);
-
+*/
 
 
     //finish = new JButton(ene.name);
--- src/Rolicom.java
+++
@@ -1,8 +0,0 @@
-class Rolicom extends Enemy {
-  {
-  at += 3;        // 敵の攻撃
-  exp *= 100;       // 経験値
-  mdf = 2;       // 敵の魔法守備力
-  name = "ロリコン";
-  }
-}
