6e01417dfd9a279937dc26d6a2231d57cbd6f6a2
sekishin <s14t242@stmail.eng.kagawa-u.ac.jp>
2015-10-07 18:48:25 +0900

    GUI

--- src/Frame.java
+++ src/Frame.java
@@ -11,21 +11,34 @@ public class Frame extends JFrame/*implements ActionListener*/ {
   JLabel tittle_logo;
   JButton finish;
   JPanel imgpanel;
+  JPanel txtpanel;
+  JPanel cmdpanel;
+  JPanel infopanel;
   GamePanel panel = new GamePanel();
 
   void createFrame (Enemy ene) {
     content = new JFrame("View JFrame");
-    content.setSize(800,800);
+    content.setSize(800,600);
+    content.setResizable(false);//ここでフレームのサイズを変更できないように設定しています。
     content.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     content.setVisible(true);
 
     tittle_logo = new JLabel("lkda", SwingConstants.CENTER);
     tittle_logo.setFont(new Font("Century", Font.ITALIC, 30));
     tittle_logo.setBounds(70, 50, 20, 40);
-    content.add(tittle_logo);
+    //content.add(tittle_logo);
 
     imgpanel = panel.imgpanel();
     content.add(imgpanel);
+
+    txtpanel = panel.txtpanel();
+    content.add(txtpanel);
+
+    cmdpanel = panel.cmdpanel();
+    content.add(cmdpanel);
+
+    infopanel = panel.infopanel();
+    content.add(infopanel);
 /*
     ImageIcon  img;
     img = new ImageIcon("C:\\Eclipse\\LKDA\\bin\\lolicom.jpg");
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,5 @@
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import java.awt.Color;
+
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 public class GamePanel extends JPanel {
@@ -7,32 +7,43 @@ public class GamePanel extends JPanel {
   //--- イメージパネルの設計
   JPanel imgpanel(){
     JPanel panel = new JPanel();
-    ImageIcon  img;
+    /*ImageIcon  img;
     img = new ImageIcon("seki.jpg");
     JLabel label = new JLabel(img);
     label.setBounds(10, 10, 100, 100);
-    panel.add(label);
-
+    panel.add(label);*/
     panel.setBorder(new TitledBorder("イメージパネル"));
-    panel.setBounds(10, 10, 120, 120);// パネルのサイズ・位置を設定
+    panel.setBackground(Color.YELLOW);
+    panel.setBounds(10, 10, 500, 350);// パネルのサイズ・位置を設定
     return panel;
   }
 
   //--- テキストパネルの設計
   JPanel txtpanel(){
     JPanel panel = new JPanel();
+    //JLabel label = new JLabel("Sample Text");
+    //panel.add(label);
+    panel.setBorder(new TitledBorder("テキストパネル"));
+    panel.setBackground(Color.RED);
+    panel.setBounds(10, 370, 500, 170);// パネルのサイズ・位置を設定
     return panel;
   }
 
   //--- コマンドパネルの設計
   JPanel cmdpanel(){
     JPanel panel = new JPanel();
+    panel.setBorder(new TitledBorder("コマンドパネル"));
+    panel.setBackground(Color.BLUE);
+    panel.setBounds(520, 370, 170, 170);// パネルのサイズ・位置を設定
     return panel;
   }
 
   //--- 情報パネルの設計
   JPanel infopanel(){
     JPanel panel = new JPanel();
+    panel.setBorder(new TitledBorder("情報パネル"));
+    panel.setBackground(Color.GREEN);
+    panel.setBounds(520, 10, 170, 350);// パネルのサイズ・位置を設定
     return panel;
   }
 }
